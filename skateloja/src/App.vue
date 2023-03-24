1a5e2f9d9b94882d2e6a63a2fda5e82f59a31906
felipecabesssa <felipesoares100@hotmail.com>
2023-03-24 12:34:59 -0300

    Projeto criado, limpo e backend - db.json

---
+++ skateloja/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    
+    <MenuPrincipal />
+
+    <router-view/>
+
+    <Footer />
+
+  </div>
+
+</template>
+
+<script>
+import Footer from './components/Footer.vue'
+import MenuPrincipal from './components/MenuPrincipal.vue'
+export default {
+  components: { MenuPrincipal, Footer },
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ skateloja/src/components/MenuPrincipal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div id="nav">
+            <router-link to="/">
+                <img src="" id="logo">
+            </router-link>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MenuPrincipal"
+}
+</script>
+
+<style scoped>
+
+</style>
